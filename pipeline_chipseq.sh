063baaef5ce268b6e362dd76dd2a04daf52aaa6a
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-11-26 15:16:03 +0000

    reso bug alignmt

---
+++ pipeline_chipseq.sh
@@ -0,0 +1,11 @@
+srr=$1
+ref=$2 #path to genome.fa
+dire=$3
+
+mkdir $dire/data/ $dire/results/ $dire/alignment/
+mkdir $dire/data/fastq/ $dire/data/ref/
+
+fasterq-dump $srr -O $dire/data/fastq/
+bowtie2-build $ref genome
+mv *.bt2 $dire/data/ref/
+bowtie2 -p8 '--local' '--very-sensitive-local' --no-hd --no-sq '-x' $dire/data/ref/genome -q $dire/data/fastq/$srr.fastq -S $dire/alignment/$srr.sam
