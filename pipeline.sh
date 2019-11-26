4c50337042a92fb848ed82d24a5ad48b945245a4
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-11-26 15:14:01 +0100

    deleting --split-3 option (depreciated)

--- pipeline.sh
+++ pipeline.sh
@@ -5,14 +5,14 @@ ref=$2
 dire=$3 #chemin absolu vers dossier HIC
 #Le script-shell sera exécuté dans Projet_long/
 
-mkdir $3/data $3/alignment $3/results
-mkdir $3/data/fastq $3/data/ref $3/data/obsolete
+mkdir $3/data/ $3/alignment/ $3/results/
+mkdir $3/data/fastq/ $3/data/ref/ $3/data/obsolete/
 mv $2 genome.fa
 mv genome.fa $3/data/ref
 
-fasterq-dump $1 --split-3 -O $3/data/fastq
+fasterq-dump $1 -O $3/data/fastq/
 
 bowtie2-build $3/data/ref/genome.fa genome
-mv genome* $3/data/ref
+mv genome* $3/data/ref/
 
 DIR="$3" SRR="$1" snakemake -j 8
