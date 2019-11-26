108e8d5509406c4144dca5efb742885213d25821
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-11-26 15:22:21 +0100

    cat des chr en genome.fa

--- pipeline.sh
+++ pipeline.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #avant faut créer un dossier HIC
 srr=$1
-ref=$2
 dire=$3 #chemin absolu vers dossier HIC
 #Le script-shell sera exécuté dans Projet_long/
 
-mkdir $3/data/ $3/alignment/ $3/results/
-mkdir $3/data/fastq/ $3/data/ref/ $3/data/obsolete/
-mv $2 genome.fa
-mv genome.fa $3/data/ref
+mkdir $dire/data/ $dire/alignment/ $dire/results/
+mkdir $dire/data/fastq/ $dire/data/ref/ $dire/data/obsolete/
 
-fasterq-dump $1 -O $3/data/fastq/
+cat *.fasta > genome.fa
+mv genome.fa $dire/data/ref/
+mv *.fasta $dire/data/ref/
 
-bowtie2-build $3/data/ref/genome.fa genome
-mv genome* $3/data/ref/
+fasterq-dump $srr -O $dire/data/fastq/
 
-DIR="$3" SRR="$1" snakemake -j 8
+bowtie2-build $dire/data/ref/genome.fa genome
+mv genome* $dire/data/ref/
+
+DIR="$dire" SRR="$srr" snakemake -j 8
