7aa43e41bbdf3898afd3d4a08f233734289e8ad8
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-11-30 00:43:30 +0100

    sorting and indexing for pysam

--- pipeline_chipseq.sh
+++ pipeline_chipseq.sh
@@ -5,16 +5,13 @@ dire=$3
 mkdir $dire/data/ $dire/results/ $dire/alignment/
 mkdir $dire/data/fastq/ $dire/data/ref/
 
-#fasterq-dump $srr -O $dire/data/fastq/
+fasterq-dump $srr -O $dire/data/fastq/
 
-cat $ref/*.fasta > $dire/data/ref/genome.fa
-chr=$(ls $ref/*.fasta) # contain all paths to chr.fasta
-for i in $chr; do
-    echo $i
-    bowtie2-build $i "$i"
-    mv *.bt2 $dire/data/ref/
-done
 bowtie2-build $dire/data/ref/genome.fa genome
 mv *.bt2 $dire/data/ref/
 
 bowtie2 -p8 '--local' '--very-sensitive-local' '-x' $dire/data/ref/genome -q $dire/data/fastq/$srr.fastq -S $dire/alignment/$srr.sam
+
+samtools view -Sb $dire/alignment/$srr.sam > $dire/alignment/$srr.bam
+samtools 'sort' $dire/alignment/$srr.bam > $dire/alignment/$srr.sorted.bam 
+samtools index $dire/alignment/$srr.sorted.bam
