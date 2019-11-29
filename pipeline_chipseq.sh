131e128ac51a1ebf9c870beade3e2fe7f81a9152
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-11-29 23:55:26 +0100

    indexing for al chr (pysam necessity)

--- pipeline_chipseq.sh
+++ pipeline_chipseq.sh
@@ -1,11 +1,20 @@
 srr=$1
-ref=$2 #path to genome.fa
+ref=$2 #path to chr
 dire=$3
 
 mkdir $dire/data/ $dire/results/ $dire/alignment/
 mkdir $dire/data/fastq/ $dire/data/ref/
 
-fasterq-dump $srr -O $dire/data/fastq/
-bowtie2-build $ref genome
+#fasterq-dump $srr -O $dire/data/fastq/
+
+cat $ref/*.fasta > $dire/data/ref/genome.fa
+chr=$(ls $ref/*.fasta) # contain all paths to chr.fasta
+for i in $chr; do
+    echo $i
+    bowtie2-build $i "$i"
+    mv *.bt2 $dire/data/ref/
+done
+bowtie2-build $dire/data/ref/genome.fa genome
 mv *.bt2 $dire/data/ref/
+
 bowtie2 -p8 '--local' '--very-sensitive-local' '-x' $dire/data/ref/genome -q $dire/data/fastq/$srr.fastq -S $dire/alignment/$srr.sam
