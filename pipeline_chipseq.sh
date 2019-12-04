ab6890f4412693f067c02cb51b30eb16253976d9
AnthonyJaquaniello <anthony.jaquaniello@gmail.com>
2019-12-04 20:53:08 +0000

    couplage input/real

--- pipeline_chipseq.sh
+++ pipeline_chipseq.sh
@@ -1,18 +1,29 @@
 srr=$1
-dire=$2
+input=$2
+dire=$3
+ref=$4 #path to genome.fa
 
-mkdir $dire/data/ $dire/results/ $dire/alignment/
-mkdir $dire/data/fastq/ $dire/data/ref/
+mkdir $dire/CHIPSEQ/
+mkdir $dire/CHIPSEQ/data/ $dire/CHIPSEQ/results/ $dire/CHIPSEQ/alignment/
+mkdir $dire/CHIPSEQ/data/fastq/ $dire/CHIPSEQ/data/ref/ $dire/CHIPSEQ/results/input/ $dire/CHIPSEQ/results/IP/
 
-fasterq-dump $srr -O $dire/data/fastq/
+fasterq-dump $srr -O $dire/CHIPSEQ/data/fastq/
+fasterq-dump $input -O $dire/CHIPSEQ/data/fastq/
+mv $dire/CHIPSEQ/data/fastq/$input.fastq $dire/CHIPSEQ/data/fastq/$input.input.fastq
 
-bowtie2-build $dire/data/ref/genome.fa genome
-mv *.bt2 $dire/data/ref/
+cp $ref $dire/CHIPSEQ/data/ref/genome.fa
+bowtie2-build $dire/CHIPSEQ/data/ref/genome.fa genome
+mv *.bt2 $dire/CHIPSEQ/data/ref/
 
-bowtie2 -p8 '--local' '--very-sensitive-local' '-x' $dire/data/ref/genome -q $dire/data/fastq/$srr.fastq -S $dire/alignment/$srr.sam
+bowtie2 -p8 '--local' '--very-sensitive-local' '-x' $dire/CHIPSEQ/data/ref/genome -q $dire/CHIPSEQ/data/fastq/$srr.fastq -S $dire/CHIPSEQ/alignment/$srr.sam
+bowtie2 -p8 '--local' '--very-sensitive-local' '-x' $dire/CHIPSEQ/data/ref/genome -q $dire/CHIPSEQ/data/fastq/$input.input.fastq $dire/CHIPSEQ/alignment/$input.input.sam
 
-samtools view -Sb $dire/alignment/$srr.sam > $dire/alignment/$srr.bam
-samtools 'sort' $dire/alignment/$srr.bam > $dire/alignment/$srr.sorted.bam 
-samtools index $dire/alignment/$srr.sorted.bam
+samtools view -Sb $dire/CHIPSEQ/alignment/$srr.sam > $dire/CHIPSEQ/alignment/$srr.bam
+samtools view -Sb $dire/CHIPSEQ/alignment/$input.input.sam > $dire/CHIPSEQ/alignment/$input.input.bam
+samtools 'sort' $dire/CHIPSEQ/alignment/$srr.bam > $dire/CHIPSEQ/alignment/$srr.sorted.bam
+samtools 'sort' $dire/CHIPSEQ/alignment/$input.input.bam > $dire/CHIPSEQ/alignment/$input.input.sorted.bam
+samtools index $dire/CHIPSEQ/alignment/$srr.sorted.bam $dire/CHIPSEQ/alignment/$srr.sorted.bam.bai
+samtools index $dire/CHIPSEQ/alignment/$input.input.sorted.bam $dire/CHIPSEQ/alignment/$input.input.sorted.bam.bai
 
-python3 examples_codes/peaks_extract.py $dire/alignment/$srr.sorted.bam $dire/results/
+python3 examples_codes/peaks_extract.py $dire/CHIPSEQ/alignment/$srr.sorted.bam $dire/CHIPSEQ/results/IP/
+python3 examples_codes/peaks_extract.py $dire/CHIPSEQ/alignment/$input.input.sorted.bam $dire/CHIPSEQ/results/input/
